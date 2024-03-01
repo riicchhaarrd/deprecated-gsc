be1774dda6e398ddf1c48bbd166fbd3280535a97
Richard <5922568+riicchhaarrd@users.noreply.github.com>
2024-03-01 15:36:42 +0100

    change flags

--- src/script/ast/ast_generator.cpp
+++ src/script/ast/ast_generator.cpp
@@ -888,8 +888,7 @@ namespace script
 				parse::source_map sources;
 				parse::definition_map definitions;
 				parse::preprocessor proc;
-				proc.set_options(parse::k_EPreprocessorOption_IncludeOnce | parse::k_EPreprocessorOption_IgnoreUnknownDirectives |
-							   parse::k_EPreprocessorOption_DoNotInclude);
+				proc.set_options(parse::k_EPreprocessorOption_IgnoreUnknownDirectives);
 				proc.set_include_path_extension(".gsc");
 				parse::lexer_opts opts;
 
